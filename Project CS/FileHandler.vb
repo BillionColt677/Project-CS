80be0293991b8d9b90bd62f5611f623fde35711c
miqda <miqda@DESKTOP-D5RVH1J>
2025-09-09 09:01:40 +0100

    Add project files.

---
+++ Project CS/FileHandler.vb
@@ -0,0 +1,3 @@
+﻿Module FileHandler
+
+End Module
---
+++ Project CS/My Project/AssemblyInfo.vb
@@ -0,0 +1,32 @@
+﻿Imports System
+Imports System.Reflection
+Imports System.Runtime.InteropServices
+
+' General Information about an assembly is controlled through the following
+' set of attributes. Change these attribute values to modify the information
+' associated with an assembly.
+
+' Review the values of the assembly attributes
+
+<Assembly: AssemblyTitle("Project CS")>
+<Assembly: AssemblyDescription("")>
+<Assembly: AssemblyCompany("Atlas Playbook v0.4.1")>
+<Assembly: AssemblyProduct("Project CS")>
+<Assembly: AssemblyCopyright("Copyright © Atlas Playbook v0.4.1 2025")>
+<Assembly: AssemblyTrademark("")>
+
+<Assembly: ComVisible(False)>
+
+'The following GUID is for the ID of the typelib if this project is exposed to COM
+<Assembly: Guid("37b1e98c-058b-45d2-9c96-e1e71488127c")>
+
+' Version information for an assembly consists of the following four values:
+'
+'      Major Version
+'      Minor Version
+'      Build Number
+'      Revision
+'
+
+<Assembly: AssemblyVersion("1.0.0.0")>
+<Assembly: AssemblyFileVersion("1.0.0.0")>
